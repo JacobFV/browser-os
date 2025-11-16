0356dfea422654dcf45a2381379d8d21bc7b94bc
Jacob Valdez <jacobfv123@gmail.com>
2025-11-15 17:04:24 -0800

    desktop color

--- packages/os/src/Desktop.tsx
+++ packages/os/src/Desktop.tsx
@@ -4,7 +4,20 @@ import './Desktop.css';
 export const Desktop: React.FC = () => {
   return (
     <div className="os-desktop">
-      <div className="os-desktop-background" />
+      <div className="os-desktop-background">
+        <div className="os-desktop-branding">
+          <img src="/favicon.svg" alt="Browser OS Icon" className="os-desktop-icon" />
+          <span className="os-desktop-title">BROWSER-OS</span>
+        </div>
+        <div className="os-desktop-blurstorm">
+          <div className="blur-blob blob-1"></div>
+          <div className="blur-blob blob-2"></div>
+          <div className="blur-blob blob-3"></div>
+          <div className="blur-blob blob-4"></div>
+          <div className="blur-blob blob-5"></div>
+          <div className="blur-blob blob-6"></div>
+        </div>
+      </div>
       <div className="os-desktop-content">
         {/* Desktop icons, widgets, etc. can go here */}
       </div>
